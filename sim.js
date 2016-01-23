bbc86a9f132a54cdef9d78a897da5971ed893863
Brian Black <black@gatech.edu>
2016-01-23 18:50:45 -0500

    surfnorm calcd for air pixels, not solid pixels

--- sim.js
+++ sim.js
@@ -31,60 +31,47 @@ define(function(require){
         // move this many along the short:
         var shortPerLong = short / long;
         assert(Math.abs(shortPerLong) >= 0 && Math.abs(shortPerLong) <= 1);
-        var row;
-        var col;
-        var lastRow;
-        var lastCol;
+        var row = p.y;
+        var col = p.x;
+        var nextRow;
+        var nextCol;
         var collision;
         for (var i = 0; Math.abs(i) < Math.abs(long); i += (long > 0 ? 1 : -1)) {
             p.add(yIsLonger ? yDir : xDir); // move 1 unit along longer axis
             p[yIsLonger ? 'x' : 'y'] += shortPerLong; // and less along shorter
             // p now has int component on long axis; float component on short.
-            col = Math.round(p.x);
-            row = Math.round(p.y);
-            if (this.world.land[row][col]) {
+            nextCol = Math.round(p.x);
+            nextRow = Math.round(p.y);
+            if (this.world.land[nextRow][nextCol]) {
                 collision = true;
                 break;
             }
-            lastRow = row;
-            lastCol = col;
-        }
-        function stepUntilNoLand(from, step, land){
-            var pos = from.clone();
-            var posNext = pos.clone();
-            var limit = 2;
-            for (var i=0; i<limit; i++){
-                if (land[posNext.y][posNext.x]) {
-                    posNext.subtract(step);
-                } else {
-                    break;
-                }
-            }
-            return pos;
+            row = nextRow;
+            col = nextCol;
         }
         if (collision) {
             // position ent just before land
-            p.copy({x: lastCol, y: lastRow});
+            p.copy({x: col, y: row});
             ent.position = p;
-            // find surfnorm to affect v. first pixel into the surface:
-            var slopeRef = p.clone().add(xDir).add(yDir);
-            // trace backward a short distance along X:
-            var slopeStart = stepUntilNoLand(slopeRef, xDir, this.world.land);
-            var slopeEnd = stepUntilNoLand(slopeRef, yDir, this.world.land);
-            var slope = slopeEnd.clone().subtract(slopeStart);
+            var surfnorm = new Victor(0, 0);
+            for (var dx = -1; dx <= 1; dx++) {
+                for (var dy = -1; dy <= 1; dy++) {
+                    if (this.world.land[p.y + dy][p.x + dx]) {
+                        surfnorm.add(new Victor(dx, dy).invert().norm());
+                    }
+                }
+            }
+            surfnorm.norm();
+            // assert(surfnorm.dot(v) <= 1e-9);
             // TODO: use rotateBy. in current Victor, they are swapped.
-            var surfnorm = slope.clone().rotate(Math.PI/2).norm();
-            // we decide which way the surfnorm points by assuming it's opposite
-            // the velocity vector
-            if (surfnorm.dot(v) >= 0)
-                surfnorm.invert();
-            assert(surfnorm.dot(v) <= 0);
-            // TODO: use rotateBy. in current Victor, they are swapped.
-            var newV = new Victor(v.x, v.y)
+            var newV = v.clone()
                 .rotate(surfnorm.verticalAngle())
                 .invertX()
                 .rotate(-surfnorm.verticalAngle())
                 .invert();
+            var RESTITUTION = 1;
+            newV.x = newV.x * RESTITUTION;
+            newV.y = newV.y * RESTITUTION;
             ent.velocity.copy(newV);
         }
     };
